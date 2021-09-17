c79b97790f40889f3853d9b553d4e7fae5ada6c1
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-17 16:14:32 -0700

    'area' code complete
    with validation

--- scripts/scripts-area.js
+++ scripts/scripts-area.js
@@ -1,1 +1,17 @@
-console.log("started"); 
+let base = document.querySelector("#base");
+let height = document.querySelector("#height");
+let buttonArea = document.querySelector("#btn-area");
+let showOutput = document.querySelector("#output-area"); 
+
+function calculateArea() {
+    if (base.value <=0 || height.value <=0) {
+        showOutput.innerText = "Please enter a valid number"; 
+    } else {
+        let ourArea = (base.value * height.value)/2;
+        console.log(ourArea + ' is our area');
+        showOutput.innerHTML = `Area for this triangle is ${ourArea} cm<sup> 2</sup>` ;
+    }    
+}
+
+
+buttonArea.addEventListener("click", calculateArea);
