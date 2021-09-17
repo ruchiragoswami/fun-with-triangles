12237e5b7cd9525653719ce49d88d33ec5556afd
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-17 15:29:57 -0700

    Update scripts-hypo.js
    validation added

--- scripts/scripts-hypo.js
+++ scripts/scripts-hypo.js
@@ -1,28 +1,33 @@
 let side1 = document.querySelector("#side-1");
 let side2 = document.querySelector("#side-2");
 let btnHypo = document.querySelector("#btn-hypo");
-let showResult = document.querySelector("#output-hypo"); 
+let showResult = document.querySelector("#output-hypo");
 
 
 
 function calculateHypotenuse() {
+    if (side1.value <= 0 || side2.value <= 0) {
+        showResult.innerText = "Please enter a valid number";
+
+    } else {
+        let a = side1.value * side1.value;
+        let b = side2.value * side2.value;
+        console.log(a + b);
+        let c = Math.sqrt(a + b);
+        let d = c.toFixed(2);
+        console.log(c);
+        console.log(d);
+
+        showResult.innerText = `The length of hypotenuse is ${d}`;
+
+    }
 
 
-    
 
-    let a = side1.value * side1.value;
-    let b = side2.value * side2.value;
-    console.log(a+b);
-    let c = Math.sqrt(a+b); 
-    let d = c.toFixed(2); 
-    console.log(c); 
-    console.log(d); 
 
-    showResult.innerText = `The length of hypotenuse is ${d}`; 
 
 }
 
 
 
 btnHypo.addEventListener("click", calculateHypotenuse);
-
