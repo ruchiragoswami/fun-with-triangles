58e7cbdafc579946f04235e3b081f8f4daad90db
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-16 20:51:15 -0700

    querySelectorAll
    Understanding how querySelectorAll works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,45 @@
 console.log("started");
 
 
-let angle1 = document.querySelector("#angle1");
-let angle2 = document.querySelector("#angle2");
-let angle3 = document.querySelector("#angle3"); 
+// let angle1 = document.querySelector("#angle1");
+// let angle2 = document.querySelector("#angle2");
+// let angle3 = document.querySelector("#angle3"); 
 let allAngles = document.querySelectorAll(".angle");
 let isTriangle = document.querySelector("#btn-checkTri");
 
-// console.log(allAngles.value); 
- 
 
-// function checkTriangle(angle1.value, angle2.value, angle3) {
-    
-//     console.log(angle1.value + " is angle one");
-//     let sumAngles=  angle1 + angle2 + angle3;
-//     console.log(sumAngles);
 
+
+// function checkTriangle() {
+//     for (let i=0; i < allAngles.length; i++) {
+//         allAngles[i] = Number(allAngles[i]);
+//         // console.log(allAngles[i].value);  
+//     //    Number(allAngles[i].value) ;
+//         let sumOfAngles = 0;
+//         sumOfAngles = sumOfAngles + allAngles[i].value;    
+//         return sumOfAngles;      
+//         console.log(sumOfAngles + " is sum of angles");
+//     }   
 // }
 
 
-function checkTriangle() {
-    for (let i=0; i < allAngles.length; i++) {
-        console.log(allAngles[i].value);
-    }
+function calculateSumOfAngles() {
+    console.log(allAngles[0].value); 
+    let x=  Number(allAngles[0].value);
+    console.log(typeof(x));
+
+    // let angleArray = [];
+    // angleArray.push(allAngles.value); 
+    // console.log(angleArray); 
+
+    // for (let i =0; i < allAngles.length; i++) {
+    //     Number(allAngles.value);
+    //     let sum = 0;
+    //     sum = sum + allAngles[i].value ;  
+    //     return sum; 
+    //     console.log("sume is " + sum);       
+    // }
+    
 }
 
-
-isTriangle.addEventListener("click", checkTriangle);
+isTriangle.addEventListener("click", calculateSumOfAngles);
