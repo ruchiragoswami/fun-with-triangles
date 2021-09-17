f082d42112c0fc41597961a4077e59c868455901
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-17 07:53:21 -0700

    Update scripts.js
    obtained sum from three inputs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,20 +9,45 @@ let isTriangle = document.querySelector("#btn-checkTri");
 let showOutput = document.querySelector("#showOutput");  
 
 let myArray = [];
+let sum = 0;
 
 function extractNumber() {
     for (let i=0; i < allAngles.length; i++) {
        let x=  Number(allAngles[i].value); 
+        
+
        console.log(typeof(x) + "" + x); 
        myArray.push(x); 
     }
     console.log(myArray);
+    calculateSumOfAngles(myArray, sum); 
+
 }
 
 
 
-function calculateSumOfAngles() {
-    // console.log(allAngles[0].value); 
+function calculateSumOfAngles(someArray, sum) {
+    for (let i=0; i < someArray.length; i++) {      
+        sum = sum + someArray[i];       
+    }
+    console.log("total is " + sum); 
+
+    if (sum === 180) {
+        console.log("Triangle Yes");
+    } else {
+        console.log("No"); 
+    }
+}
+
+isTriangle.addEventListener("click", extractNumber );
+
+// calculateSumOfAngles
+
+
+
+/*
+
+// console.log(allAngles[0].value); 
     // let x=  Number(allAngles[0].value);
     
     // console.log(typeof(x));
@@ -35,8 +60,5 @@ function calculateSumOfAngles() {
         console.log(sum + " is the sum");
     }   
      
-}
 
-isTriangle.addEventListener("click", extractNumber );
-
-// calculateSumOfAngles
+*/
