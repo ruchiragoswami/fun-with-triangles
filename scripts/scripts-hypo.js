d04e0a7a6d0799b887062627ec1d4d127ac4e13e
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-17 15:51:50 -0700

    file for 'area' created
    html, js files for the 'area' page made & basic code added

--- scripts/scripts-hypo.js
+++ scripts/scripts-hypo.js
@@ -3,31 +3,17 @@ let side2 = document.querySelector("#side-2");
 let btnHypo = document.querySelector("#btn-hypo");
 let showResult = document.querySelector("#output-hypo");
 
-
-
 function calculateHypotenuse() {
     if (side1.value <= 0 || side2.value <= 0) {
         showResult.innerText = "Please enter a valid number";
-
     } else {
         let a = side1.value * side1.value;
-        let b = side2.value * side2.value;
-        console.log(a + b);
+        let b = side2.value * side2.value;       
         let c = Math.sqrt(a + b);
-        let d = c.toFixed(2);
-        console.log(c);
-        console.log(d);
+        let d = c.toFixed(2);        
 
         showResult.innerText = `The length of hypotenuse is ${d}`;
-
     }
-
-
-
-
-
 }
-
-
 
 btnHypo.addEventListener("click", calculateHypotenuse);
