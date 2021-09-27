19aabb0bdfab8454e7fc65c512262fcca0ef389b
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-27 11:36:20 -0700

    tried validating inputs
    Try 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,43 @@
- 
-let allAngles = document.querySelectorAll(".angle");
-let checkTriangle = document.querySelector("#btn-checkTri");
-let showOutput = document.querySelector("#showOutput");  
+ let allAngles = document.querySelectorAll(".angle");
+ let checkTriangle = document.querySelector("#btn-checkTri");
+ let showOutput = document.querySelector("#showOutput");
 
-let myArray = [];
-let sum = 0;
+ let myArray = [];
+ let sum = 0;
 
-function extractNumber() {
-    for (let i=0; i < allAngles.length; i++) {
-       let x=  Number(allAngles[i].value);       
-    //    console.log(typeof(x) + "" + x); 
-       myArray.push(x);     }
-    // console.log(myArray);
-    calculateSumOfAngles(myArray, sum); 
-}
+ function extractNumber() {
+    for (let i = 0; i < allAngles.length; i++) {
 
-
-function calculateSumOfAngles(someArray, sum) {
-    for (let i=0; i < someArray.length; i++) {      
-        sum = sum + someArray[i];       
+        // if (allAngles[i].value <= 0 || allAngles[i].value == "") {
+        //     showOutput.innerText = "Please enter valid inputs";
+        //     console.log(allAngles[i].value); 
+        // } else {
+        let x = Number(allAngles[i].value);
+        console.log("type of " + typeof (x) + "" + x); 
+               
+        // }
+        myArray.push(x);
+        calculateSumOfAngles(myArray, sum);
     }
-    // console.log("total is " + sum); 
 
-    if (sum === 180) {
-        showOutput.innerText = "Yes Triangle";
-        console.log("Triangle Yes");
-    } else {
-        showOutput.innerText = "No, triangle"; 
-        console.log("No"); 
-    }
-    myArray = [];
-}
-
-checkTriangle.addEventListener("click", extractNumber );
+ }
 
 
+ function calculateSumOfAngles(someArray, sum) {
+     for (let i = 0; i < someArray.length; i++) {
+         sum = sum + someArray[i];
+     }
 
+     console.log("total is " + sum);
+
+     if (sum === 180) {
+         showOutput.innerText = "Yes, these angles will form a triangle!";
+         // console.log("Yes, these angles will form a triangle!");
+     } else {
+         showOutput.innerText = "Sorry, triangle can't be formed";
+         // console.log("No"); 
+     }
+     myArray = [];
+ }
+
+ checkTriangle.addEventListener("click", extractNumber);
