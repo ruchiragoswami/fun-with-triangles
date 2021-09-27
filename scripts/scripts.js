dbb583ba692d09f8f55147bffcbea0d6d401c54b
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-27 12:27:47 -0700

    validation continued
    Kept trying and saving

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,54 @@
- 
 let allAngles = document.querySelectorAll(".angle");
 let checkTriangle = document.querySelector("#btn-checkTri");
-let showOutput = document.querySelector("#showOutput");  
+let showOutput = document.querySelector("#showOutput");
 
 let myArray = [];
-let sum = 0;
+// let sum = 0;
+
 
 function extractNumber() {
-    
-
-    for (let i=0; i < allAngles.length; i++) {
-        
-        let x=  Number(allAngles[i].value);       
-        //   console.log(typeof(x) + "" + x);                
-        myArray.push(x); 
-
-        for (let j=0; j < myArray.length; j++) {
-            if (j <= 0 || j == "") {
-                showOutput.innerText = "valid?";
-            } else {
-                calculateSumOfAngles(myArray, sum); 
-            }
-        }        
-        
-
+    for (let i = 0; i < allAngles.length; i++) {
+        if (allAngles[i].value <= 0 || allAngles[i].value === "") {
+            console.log("valid?"); 
+            showOutput.innerText = "Valid?"                     
+            break; 
+        } else {
+            let x = Number(allAngles[i].value);
+            myArray.push(x); 
+        }
+        calculateSumOfAngles(myArray);
     }
-    // console.log(myArray);
-    // calculateSumOfAngles(myArray, sum); 
 }
 
+     // function extractNumber() {
 
-function calculateSumOfAngles(someArray, sum) {
-    for (let i=0; i < someArray.length; i++) {      
-        sum = sum + someArray[i];       
+
+     //     for (let i=0; i < allAngles.length; i++) {
+
+     //         let x=  Number(allAngles[i].value);       
+     //         //   console.log(typeof(x) + "" + x);                
+     //         myArray.push(x); 
+
+     //         for (let j=0; j < myArray.length; j++) {
+     //             if (j <= 0 || j == "") {
+     //                 showOutput.innerText = "valid?";
+     //             } else {
+     //                 calculateSumOfAngles(myArray, sum); 
+     //             }
+     //         }        
+
+
+     //     }
+     //     // console.log(myArray);
+     //     // calculateSumOfAngles(myArray, sum); 
+     // }
+
+
+function calculateSumOfAngles(someArray) {
+    let sum = 0;
+
+    for (let i = 0; i < someArray.length; i++) {
+        sum = sum + someArray[i];
     }
     // console.log("total is " + sum); 
 
@@ -40,13 +56,10 @@ function calculateSumOfAngles(someArray, sum) {
         showOutput.innerText = "Yes, these angles will form a triangle!";
         // console.log("Yes, these angles will form a triangle!");
     } else {
-        showOutput.innerText = "Sorry, triangle can't be formed"; 
+        showOutput.innerText = "Sorry, triangle can't be formed";
         // console.log("No"); 
     }
     myArray = [];
 }
 
-checkTriangle.addEventListener("click", extractNumber );
-
-
-
+checkTriangle.addEventListener("click", extractNumber);
